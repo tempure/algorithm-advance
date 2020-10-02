2ef77d889d786b583c7a81d886c4b8c1d39db540
lazydooog <444836458@qq.com>
2020-10-02 17:04:21 +0800

    NOI1999 二维区间DP/记忆化搜索

--- 动态规划/区间DP/acwing_321.cpp
+++ 动态规划/区间DP/acwing_321.cpp
@@ -2,7 +2,7 @@
 * @Description: 321. 棋盘分割
 * @Author: Xiaobin Ren
 * @Date:   2020-10-02 16:39:19
-* @Last Modified time: 2020-10-02 16:39:33
+* @Last Modified time: 2020-10-02 17:04:04
 */
 #include <iostream>
 #include <algorithm>
@@ -16,12 +16,14 @@ const int N = 15, M = 9;
 const double INF = 1e9;
 int n, m = 8;
 int s[M][M];
+//f[x1][y1][x2][y2][k]表示子矩阵(x1,y1,x2,y2) 切分为k部分的所有方案
 double f[M][M][M][M][N];
 double X; //平均数
 
 
-double get(int x1, int y1, int x2, int y2){
-    double sum = s[x2][y2] - s[x2][y1 -1] - s[x1-1][y2] + s[x1 -1][y1-1] - X;//(xi连加减去平均数)
+double get(int x1, int y1, int x2, int y2){  //计算一个小块的(xi - X)^ 2 / n
+    //直接用题目给的公式算 不用变形公式
+    double sum = s[x2][y2] - s[x2][y1 -1] - s[x1-1][y2] + s[x1 -1][y1-1] - X;
     return sum * sum / n;
 }
 
@@ -31,12 +33,12 @@ double dp(int x1, int y1, int x2, int y2, int k){
     if(k == 1) return v = get(x1, y1, x2, y2); // 不能再切了 最小了
     
     v = INF;
-    for(int i = x1; i < x2; i++){ //横切
-        v = min(v, dp(x1,y1,i,y2,k -1) + get(i + 1, y1, x2, y2)); //选上面
-        v = min(v, dp(i + 1, y1, x2, y2,k-1) + get(x1,y1,i,y2)); //选下边
+    for(int i = x1; i < x2; i++){ //横切 枚举行坐标 
+        v = min(v, dp(x1,y1,i,y2,k -1) + get(i + 1, y1, x2, y2)); //枚举上面 然后下边直接计算 然后加上
+        v = min(v, dp(i + 1, y1, x2, y2,k-1) + get(x1,y1,i,y2)); //选下边 同理
     }
     
-    for(int i = y1; i < y2; i++){
+    for(int i = y1; i < y2; i++){ //纵切
         v = min(v, dp(x1, y1,x2,i,k-1) + get(x1,i+1,x2,y2));
         v = min(v, dp(x1,i+1,x2,y2,k-1) + get(x1, y1,x2,i));
     }
