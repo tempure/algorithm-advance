27cd32cd0eedaf0321cb83330e340be0065eb395
lazydooog <xbren@bupt.edu.cn>
2020-11-03 22:47:36 +0800

    树上倍增LCA/Kruskal/次小生成树

---
+++ 图论/最近公共祖先/acwing_352.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for(int i = a; i< n; i++)
+#define per(i,a,n) for(int i=n-1; i>=a; i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+//#define x first
+//#define y second
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res=0;a%=mod;assert(b>=0);for(;b;b>>=1){if(b&1)res=(res+a)%mod;a=2*a%mod;}return res;}
+ll powmod(ll a, ll b) {ll res=1;a%=mod;assert(b>=0);for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//snippet-head
+
+
