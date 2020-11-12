d042405aeb2e0434ab613b1130321165fcda257d
lazydooog <xbren@bupt.edu.cn>
2020-11-12 21:56:20 +0800

    JSOI2008/线段树/区间最值

--- 数据结构/线段树/acwing_1275.cpp
+++ 数据结构/线段树/acwing_1275.cpp
@@ -15,84 +15,68 @@ ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) 
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
 //snippet-head
 
-//单点修改不需要延迟标记 区间修改才需要 单点只需要pushup操作
-//区间问题重点思考如何用子节点来维护父节点
-//记录左子节点的最大后缀和 和 右边子节点的最大前缀和
-//那么父节点的最大子段和就是max{左子节点最大子段和, 右子节点最大子段和 , 左子节点最大后缀+右子节点最大前缀}
-//这个过程是递归的 回溯的时候 子节点只会pushup自己段的最大前后缀给父节点
+//直接在1~m区间建立线段树 每次末尾增加就是单点修改
+//最大值就是区间查询
 
-const int N = 5e5 + 10;
+const int N = 200010;
 
-int n, m;
-int w[N]; //序列
+int m, p;
 
 struct Node {
-    int l, r;
-    int sum; //当前段的区间和
-    int lmax, rmax; //子节点最大前后缀
-    int tmax; //当前段的最大子段和
+    int l, r ;
+    int v; //区间[l,r] 的最大值
 } tr[N * 4];
 
-void pushup(Node &u, Node &l, Node & r) {
-    u.sum = l.sum + r.sum;
-    u.lmax = max(l.lmax, l.sum + r.lmax); //维护
-    u.rmax = max(r.rmax, r.sum + l.rmax);
-    u.tmax = max(max(l.tmax, r.tmax), l.rmax + r.lmax);
+//子节点更新父节点
+void pushup(int u) {
+    tr[u].v = max(tr[u << 1].v, tr[u << 1 | 1].v);
 }
 
-void pushup(int u) {
-    pushup(tr[u], tr[u << 1], tr[u << 1 | 1]);
+int query(int u, int l, int r) {
+    if (tr[u].l >= l && tr[u].r <= r) return tr[u].v; //查询的区间完全包含在树中的某个段区间内 段的最大值就是候选值
+
+    int mid = tr[u].l + tr[u].r >> 1;
+    int v = 0;
+    if (l <= mid) v = query(u << 1, l, r); // 左边 有交集 递归向下查左边 注意l <= mid
+    if (r > mid) v = max(v, query(u << 1 | 1, l , r)); //右边有交集 递归查右边
+    return v;
+}
+
+void modify(int u, int x, int v) { //将x位置的点的值修改为v
+    if (tr[u].l == x && tr[u].r == x) tr[u].v = v; //叶节点找到了直接修改
+    else {
+        int mid = tr[u].l + tr[u].r >> 1;
+        if (x <= mid) modify(u << 1, x , v);
+        else modify(u << 1 | 1, x, v);
+        pushup(u); //修改之后要更新最大值 回溯的时候
+    }
 }
 
 void build(int u, int l, int r) {
-    tr[u].l = l, tr[u].r = r;
-    if (l == r) tr[u] = {l, r, w[r], w[r], w[r], w[r]};
-    else {
-        int mid = l + r >> 1;
-        build(u << 1, l, mid), build(u << 1 | 1, mid + 1, r);
-        pushup(u);
-    }
-}
-
-int modify(int u, int x, int v) {
-    if (tr[u].l == x && tr[u].r == x) tr[u] = {x, x, v, v, v, v};
-    else {
-        int mid = tr[u].l + tr[u].r >> 1;
-        if (x <= mid) modify(u << 1, x, v);
-        else modify(u << 1 | 1, x, v);
-        pushup(u);
-    }
-}
-
-Node query(int u, int l, int r) {
-    if (tr[u].l >= l && tr[u].r <= r) return tr[u];
-    else {
-        int mid = tr[u].l + tr[u].r >> 1;
-        if (r <= mid) return query(u << 1, l, r);
-        else if (l > mid) return query(u << 1 | 1, l , r);
-        else {
-            auto left = query(u << 1, l , r);
-            auto right = query(u << 1 | 1, l, r);
-            Node res;
-            pushup(res, left, right);
-            return res;
-        }
-    }
+    tr[u] = {l, r};
+    if (l == r) return ;
+    int mid = l + r >> 1;
+    build(u << 1, l, mid), build(u << 1 | 1, mid + 1, r);
 }
 
 int main() {
-    scanf("%d%d", &n, &m);
-    for (int i = 1; i <= n; i++) scanf("%d", &w[i]);
-    build(1, 1, n);
+    int n = 0, last = 0; //上一次查询的结果
+    scanf("%d%d", &m, &p);
+    build(1, 1, m);
 
-    int k, x, y;
+    char op[2];
+    int x;
+    int l;
     while (m --) {
-        scanf("%d%d%d", &k, &x, &y);
-        if (k == 1) {
-            if (x > y) swap(x, y);
-            printf("%d\n", query(1, x, y).tmax);
+        scanf("%s%d", op, &x);
+        if (*op == 'Q') {
+            last = query(1, n - x + 1, n);
+            printf("%d\n", last);
         }
-        else modify(1, x, y);
+        else {
+            modify(1, n + 1, (last + x) % p);
+            n ++;
+        }
     }
 
     return 0;
