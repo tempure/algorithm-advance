f03a4dda550aafa67a118e1d376b0b7d1de70a2a
lazydooog <xbren@bupt.edu.cn>
2020-11-18 16:48:54 +0800

    O(1)快速乘/高精度

---
+++ 基础算法/位运算/acwing_90_2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//snippet-head
+
+//O(1)快速乘 水掉64位高精度
+
+inline ll fastmul(ll x, ll y, ll p) {
+    return (x * y - (ll)((long double)x / p * y) * p + p) % p;
+}
+
+int main()
+{
+    ll a, b, p;
+    cin >> a >> b >> p;
+    cout << fastmul(a, b, p);
+}
