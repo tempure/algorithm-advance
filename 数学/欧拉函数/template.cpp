ed9859780d654c84e0a76591ad2d877fa33b0d47
lazydooog <xbren@bupt.edu.cn>
2020-11-20 19:01:36 +0800

    线性筛求欧拉函数模板

---
+++ 数学/欧拉函数/template.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//snippet-head
+
+//线性筛 求欧拉函数模板
+
+const int N = 1010;
+
+int n;
+int primes[N], cnt;
+int phi[N]; //欧拉函数
+bool v[N];
+
+void get_euler(int n) { //线性筛求欧拉函数
+    memset(v, 0, sizeof v);
+    cnt = 0;
+
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        if (!v[i]) {
+            primes[cnt++] = i;
+            phi[i] = i - 1;
+        }
+        for (int j = 0; primes[j] <= n / i; j++) {
+            int t = primes[j] * i;
+            v[t] = 1;
+            if (i % primes[j] == 0) {
+                phi[t] = phi[i] * primes[j];
+                break;
+            }
+            phi[t] = phi[i] * (primes[j] - 1);
+        }
+    }
+}
