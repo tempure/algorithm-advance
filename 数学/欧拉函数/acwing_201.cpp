5655fbf1fd0c734910608e1ad4978d7059d4660a
lazydooog <xbren@bupt.edu.cn>
2020-11-20 19:00:42 +0800

    POJ3090/欧拉函数/线性筛/GCD

---
+++ 数学/欧拉函数/acwing_201.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//snippet-head
+
+
+const int N = 1010;
+
+int n;
+int primes[N], cnt;
+int phi[N]; //欧拉函数
+bool v[N];
+
+void get_euler(int n) { //线性筛求欧拉函数
+    memset(v, 0, sizeof v);
+    cnt = 0;
+
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        if (!v[i]) {
+            primes[cnt++] = i;
+            phi[i] = i - 1;
+        }
+        for (int j = 0; primes[j] <= n / i; j++) {
+            int t = primes[j] * i;
+            v[t] = 1;
+            if (i % primes[j] == 0) {
+                phi[t] = phi[i] * primes[j];
+                break;
+            }
+            phi[t] = phi[i] * (primes[j] - 1);
+        }
+    }
+}
+
+
+int main() {
+    int t; cin >> t;
+    int kase = 1;
+    while (t --) {
+        cin >> n;
+        get_euler(n);
+        ll res = 0;
+        for (int i = 2; i <= n; i++) res += phi[i];
+        res = 3 + 2 * res;
+        printf("%d %d %lld\n", kase++, n, res);
+    }
+
+    return 0;
+}
