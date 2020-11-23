6e67b47e8578fce26b03b18fe9640bb79922e2a9
lazydooog <xbren@bupt.edu.cn>
2020-11-23 18:17:19 +0800

    矩阵乘法模板

--- 数学/矩阵乘法/template.cpp
+++ 数学/矩阵乘法/template.cpp
@@ -23,15 +23,17 @@ void mul(int c[], int a[], int b[][N]) { //c = a  * b 一维乘二维
     int temp[N] = {0};
     for (int i = 0; i < N; i++) //列
         for (int j = 0; j < N; j++) //行
-            temp[i] = (temp[i] + (ll)a[j] * b[j][i]) % m;
+            temp[i] = (temp[i] + (ll)a[j] * b[j][i]); //这里可选是否取模
     memcpy(c, temp, sizeof temp); //这里要写temp  因为传进来的c只是一个指针
 }
 
+//对于一行的矩阵*二维的矩阵
+//可以将一行的矩阵初始化为二维的，除了第一行其余 行为0即可
 void mul(int c[][N], int a[][N], int b[][N]) { //c = a * b //二维相乘
     int temp[N][N] = {0};
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             for (int k = 0; k < N; k++)
-                temp[i][j] = (temp[i][j] + (ll)a[i][k] * b[k][j]) % m;
+                temp[i][j] = (temp[i][j] + (ll)a[i][k] * b[k][j]);//此处可选取模 % m;
     memcpy(c, temp, sizeof temp);
 }
