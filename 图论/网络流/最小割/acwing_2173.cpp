cac9ccada02661a3dbc30783aaf4faf9231593af
xbren <xbren@bupt.edu.cn>
2020-12-30 17:21:29 +0800

    最小割模板题

---
+++ 图论/网络流/最小割/acwing_2173.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+//最大流==最小割
+//这个模板，在S无法到达T的时候求出的最大流答案就是0，不需要再判断连通性
+
+const int N = 10010, M = 2e5 + 10, INF = 0x3f3f3f3f;
+int h[N], ne[M], e[M], f[M], idx;
+int n, m, S, T;
+int q[N], d[N], cur[N]; //d是分层图层数 cur是当前弧优化
+
+void add(int a, int b, int c) {
+    e[idx] = b, ne[idx] = h[a], f[idx] = c, h[a] = idx ++;
+    e[idx] = a, f[idx] = 0, ne[idx] = h[b], h[b] = idx ++;
+}
+
+bool bfs() {
+    int hh = 0, tt = 0;
+    memset(d, -1, sizeof d);
+    q[0] = S, d[S] = 0, cur[S] = h[S];
+    while (hh <= tt) {
+        int t = q[hh++];
+        for (int i = h[t]; i != -1; i = ne[i]) {
+            int ver = e[i];
+            if (d[ver] == -1 && f[i]) {
+                d[ver] = d[t] + 1; //层数
+                cur[ver] = h[ver];
+                if (ver == T) return true;
+                q[++tt] = ver;
+            }
+        }
+    }
+    return false;
+}
+
+int find(int u, int limit) { //当前u 从S到u最大的可增加流量是limit
+    if (u == T) return limit;
+    int flow = 0; //从当前点往后流的流量最多是多少
+    for (int i = cur[u]; i != -1 && flow < limit; i = ne[i]) {
+        cur[u] = i; //当前搜的弧
+        int ver = e[i];
+        if (d[ver] == d[u] + 1 && f[i]) { //只搜下一层的点 防止环的干扰
+            int t = find(ver, min(f[i], limit - flow));
+            if (!t) d[ver] = -1; //当前边到终点没有增广路 就层数设为-1，之后不会再搜到了
+            f[i] -= t, f[i ^ 1] += t, flow += t; //存在增广路 就更新残留网络
+        }
+    }
+    return flow;
+}
+
+int dinic() {
+    int r = 0, flow;
+    while (bfs())  //bfs同时建立分层图 以及返回是否有增广路
+        while (flow = find(S, INF)) r += flow; //所有能够增广的流量累加
+
+    return r;
+}
+
+int main() {
+    scanf("%d%d%d%d", &n, &m, &S, &T);
+    memset(h, -1, sizeof h);
+    while (m --) {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        add(a, b, c);
+    }
+    printf("%d\n", dinic());
+
+    return 0;
+}
