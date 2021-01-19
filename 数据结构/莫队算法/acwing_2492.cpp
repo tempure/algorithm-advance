826375a1868534e321a4d67c456e9f23000d71a7
xbren <xbren@bupt.edu.cn>
2021-01-19 16:38:26 +0800

    SDOI2009《HH的项链》 / 朴素莫队

---
+++ 数据结构/莫队算法/acwing_2492.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+/*
+朴素莫队：不同块的询问按照左端点升序，块内按照右端点升序
+处理完一个块内所有询问的区间，才会处理下一个块
+分块区间长度的计算：假设块长为a，序列长为n，注意a是int类型
+左指针移动数量级m * a, m是询问数量
+右指针移动数量级n * n / a 让左右指针移动的次数尽量相等 解出a = sqrt(n*n/m)
+*/
+
+const int N = 50010, M = 200010, S = 1000010;
+
+int n, m, len;
+int w[N],  ans[M];
+
+struct Quert {
+    int id, l, r;
+} q[M];
+
+int cnt[S];
+
+int get(int x) {
+    return x / len;
+}
+
+bool cmp(const Quert& a, const Quert& b) {
+    int i = get(a.l), j = get(b.l);
+    if (i != j) return i < j;
+    return a.r < b.r;
+}
+
+void add(int x, int &res) {
+    if (!cnt[x]) res ++;
+    cnt[x] ++;
+}
+
+void del(int x, int &res) {
+    cnt[x] --;
+    if (!cnt[x]) res --;
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) scanf("%d", &w[i]);
+    scanf("%d", &m);
+    len = sqrt((double) n * n / m);
+
+    for (int i = 0; i < m; i++) {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        q[i] = {i, l, r};
+    }
+    sort(q, q + m, cmp);
+
+    //j是左边界指针 i是右边界指针 w[]下标都是从1开始
+    //初始i=0,j=1是为了正确计算[1,1]的结果 i,j均从0开始也可以
+    for (int k = 0, i = 0, j = 1, res = 0; k < m; k ++) {
+        int id = q[k].id, l = q[k].l, r = q[k].r;
+        while (i < r) add(w[++i], res);
+        while (i > r) del(w[i--], res);
+        while (j < l) del(w[j++], res);
+        while (j > l) add(w[--j], res);
+        ans[id] = res;
+    }
+
+    for (int i = 0; i < m; i++) printf("%d\n", ans[i]);
+    return 0;
+}
+
