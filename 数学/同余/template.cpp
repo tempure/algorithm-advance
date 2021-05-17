273017247e846db93938bd785eccc70106511916
xbren <lyvrth@gmail.com>
2021-05-17 17:35:00 +0800

    修正Exgcd模板

--- 数学/同余/template.cpp
+++ 数学/同余/template.cpp
@@ -1,39 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define sz(x) ((int)(x).size())
-typedef vector<int> vi;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-mt19937 mrand(random_device{}());
-const ll mod = 1000000007;
-int rnd(int x) { return mrand() % x;}
-ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
-ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
-//snippet-head
 
-
-//exgcd模板
-
-// long long
-
+//exgcd模板 求出 ax+by = gcd(a,b)的 x,y
+// long long 返回 gcd(a,b)
 ll exgcd(ll a, ll b, ll &x, ll & y) {
     if (!b) {
         x = 1, y = 0;
         return a;
     }
-
     ll d = exgcd(b, a % b, y, x);
     y -= a / b * x;
-
     return d;
 }
 
-//int
-
+//int 返回 gcd(a,b)
 int exgcd(int a, int b, int &x, int &y) {
     if (!b) {
         x = 1, y = 0;
@@ -46,7 +26,7 @@ int exgcd(int a, int b, int &x, int &y) {
     return d;
 }
 
-//void 版本
+//void 版本 只求x,y
 void exgcd(ll a, ll b, ll &x, ll & y) {
     if (!b)
         x = 1, y = 0;
@@ -54,4 +34,33 @@ void exgcd(ll a, ll b, ll &x, ll & y) {
         exgcd(b, a % b, y, x);
         y -= a / b * x;
     }
+}
+
+//一般的线性同余方程
+//a*x≡b(mod m)，如果无解则输出 impossible。
+typedef long long ll;
+int n;
+
+int exgcd(int a, int b, int &x, int &y) {
+    if (!b) {
+        x = 1, y  = 0;
+        return a;
+    }
+    int d = exgcd(b, a % b, y, x);
+    y -= (a / b) * x;
+    return d;
+}
+
+int main() {
+    cin >> n;
+    int a, b , m;
+    int x, y;
+    while (n --) {
+        cin >> a >> b >> m;
+        int d = exgcd(a, m, x, y);
+        if (b % d) puts("impossible");
+        else cout << (ll)x * (b / d) % (m / d) << endl; //  乘的结果可能爆int 所以用long long 存储之后再取模
+        //如果直接用int存再取模 答案错误 因为int会溢出 注意模数 m / gcd(a, m) 都是和特解x 同余的 也可以直接模m
+    }
+    return 0;
 }
