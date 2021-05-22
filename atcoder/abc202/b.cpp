f7dee7af89c0c99accafa8da93d407539e5eae3e
xbren <lyvrth@gmail.com>
2021-05-23 00:43:14 +0800

    AtCoder Beginner Contest 202 (明早再写 睡了)

---
+++ atcoder/abc202/b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+    reverse(all(s));
+
+    for (int i = 0; s[i]; i++) {
+        if (s[i] == '6')s[i] = '9';
+        else if (s[i] == '9') s[i] = '6';
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
