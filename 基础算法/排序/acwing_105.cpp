12c7c81b606266f6c726180148210bf08e53cb03
xbren <lyvrth@gmail.com>
2021-06-02 11:56:51 +0800

    BZOJ3032《七夕祭》/环形均分纸牌/中位数

--- 基础算法/排序/acwing_105.cpp
+++ 基础算法/排序/acwing_105.cpp
@@ -1,31 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,n) for(int i = a; i< n; i++)
-#define per(i,a,n) for(int i=n-1; i>=a; i--)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define fi first
-#define se second
-#define sz(x) ((int)(x).size())
-typedef vector<int> VI;
-typedef long long ll;
-typedef pair<int, int> PII;
-typedef double db;
-mt19937 mrand(random_device{}());
-const ll mod = 1000000007;
-int rnd(int x) { return mrand() % x;}
-ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
-ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
-//snippet-head
+using ll = long long;
 
 const int N = 100010;
 ll n, m, t, x, y, a[N], b[N], f[N];
 
 ll calc (ll a[], ll n) {
-    for (int i = 2; i <= n; ++i)
-    {
+    for (int i = 2; i <= n; ++i) {
         a[i] -= (a[0] / n);
         f[i] = f[i - 1] + a[i];
     }
@@ -53,7 +34,7 @@ int main() {
     for (int i = 1; i <= n; i++) a[0] += a[i];
     for (int i = 1; i <= m; i++) b[0] += b[i];
 
-    ll as = a[0] % n, bs = b[0] % m;
+    ll as = a[0] % n, bs = b[0] % m; //看看能不能整除行列，不能就不能均分
 
     if (!as && !bs) cout << "both " << calc(a, n) + calc(b, m);
     else if (!as) cout << "row " << calc(a, n);
