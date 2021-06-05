8aebee708b425aa728472949dd5438617fb10cd5
xbren <lyvrth@gmail.com>
2021-06-06 00:31:30 +0800

    AcWing第 2 场周赛

---
+++ acwing/第2场周赛/a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while (t --) {
+        int n; cin >> n;
+        bool flag = false;
+
+        for (int i = 0; i * 3 <= n; i++)
+            for (int j = 0; j  * 5 <= n; j++)
+                if ((n - i * 3 - j * 5) % 7 == 0) {
+                    flag = true;
+                    cout << i << ' ' << j << ' ' << (n - 3 * i - 5 * j) / 7 << endl;
+                    goto p;
+                }
+        p :;
+        if (!flag) cout << -1 << endl;
+    }
+
+    return 0;
+}
