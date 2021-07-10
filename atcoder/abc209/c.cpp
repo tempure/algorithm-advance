f4781576c94d7624adf07d1ed219e4cbd26305df
livrth <lyvrth@gmail.com>
2021-07-10 22:58:57 +0800

    AtCoder Beginner Contest 209（待补）

---
+++ atcoder/abc209/c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define um unordered_map
+#define pq priority_queue
+#define sz(x) ((int)(x).size())
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+const int N = 2e5 + 10;
+int a[N];
+
+void solve() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	sort(a, a + n);
+
+	ll sum = 1;
+	for (int i = 0; i < n; i++)
+		sum = sum * max(0, a[i] - i) % mod;
+	cout << sum << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int t = 1;
+	// cin >> t;
+	while (t --) solve();
+
+	return 0;
+}
