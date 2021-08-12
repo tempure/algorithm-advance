e8daef5628d36bfb78195ac538b2b0f011d1f912
livrth <lyvrth@gmail.com>
2021-08-12 10:13:21 +0800

    usaco training 6.1《邮政货车》/插头DP/高精度 [WIP]

---
+++ 基础算法/打表/acwing_1412.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+//求4行N列，网格图的有向哈密顿回路数量
+//哈密顿回路经过的是点，欧拉回路经过的是边
+
+void solve() {
+
+}
+
+int main() {
+    solve();
+    return 0;
+}
