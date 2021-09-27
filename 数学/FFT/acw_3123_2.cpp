168cc69fec8db4a7e5bc4f804c0e4fc3c6e568e5
livrth <45538642+livrth@users.noreply.github.com>
2021-09-27 12:32:18 +0800

    FFT高精度乘法

---
+++ 数学/FFT/acw_3123_2.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 300000; //A*B, A,B都是1e5位数，最高次幂2e5级别，2的整次幂最接近的为26w 开30w
+const double PI = acos(-1);
+
+struct Complex {
+    double x, y;
+    Complex operator+(const Complex &t) const {
+        return {x + t .x, y + t.y};
+    }
+    Complex operator-(const Complex &t) const {
+        return {x - t.x, y - t.y};
+    }
+    Complex operator*(const Complex &t) const {
+        return {x * t.x - y * t.y, x * t.y + y * t.x};
+    }
+} a[N], b[N]; //两个乘数的多项式
+
+char s1[N], s2[N]; //乘数
+int res[N];
+int rev[N], bit, tot;
+
+void fft(Complex a[], int inv) {
+    for (int i = 0; i < tot; i++)
+        if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int mid = 1; mid < tot; mid *= 2) {
+        auto w1 = Complex({cos(PI / mid), inv * sin(PI / mid)});
+        for (int i = 0; i < tot; i += mid * 2) {
+            auto wk = Complex({1, 0});
+            for (int j = 0; j < mid; j++, wk = wk * w1) {
+                auto x = a[i + j], y = wk * a[i + j + mid];
+                a[i + j] = x + y, a[i + j + mid] = x - y;
+            }
+        }
+    }
+}
+
+void solve() {
+    cin >> s1 >> s2;
+    int n = strlen(s1) - 1, m = strlen(s2) - 1;
+    for (int i = 0; i <= n; i++) a[i].x = s1[n - i] - '0';
+    for (int i = 0; i <= m; i++) b[i].x = s2[m - i] - '0';
+    while (1 << bit < n + m + 1) bit++;
+    tot = 1 << bit;
+    for (int i = 0; i < tot; i++)
+        rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (bit - 1));
+    fft(a, 1), fft(b, 1); //DFT
+    for (int i =  0; i < tot; i++) a[i] = a[i] * b[i];
+    fft(a, -1); // IDFT
+
+    int k = 0;
+    for (int i = 0, t = 0; i < tot || t; i++) {
+        t += a[i].x / tot + 0.5;
+        res[k++] = t % 10;
+        t /= 10;
+    }
+    while (k > 1 && !res[k - 1]) k--;
+    for (int i = k - 1; i >= 0; i --) cout << res[i];
+}
+
+int main() {
+    solve();
+    return 0;
+}
