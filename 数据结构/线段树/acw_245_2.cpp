2d1505a75b5f4ffcab14086fa7b79da009e72055
livrth <45538642+livrth@users.noreply.github.com>
2021-10-07 23:08:56 +0800

    线段树/最大连续子段和/单点修改/区间查询

---
+++ 数据结构/线段树/acw_245_2.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*202 10.7 update
+注意到query 函数 new 一个 node 然后返回
+这是因为每次query只需要保留一下查询过程中的信息，根本不不会修改节点的信息
+所以可以这样搞
+*/
+
+
+//===============old content===========================//
+
+//单点修改不需要延迟标记 区间修改才需要 单点只需要pushup操作
+//区间问题重点思考如何用子节点来维护父节点
+//记录左子节点的最大后缀和 和 右边子节点的最大前缀和
+//那么父节点的最大子段和就是max{左子节点最大子段和, 右子节点最大子段和 , 左子节点最大后缀+右子节点最大前缀}
+//这个过程是递归的 回溯的时候 子节点只会pushup自己段的最大前后缀给父节点
+
+const int N = 5e5 + 10;
+
+int n, m;
+int w[N]; //序列
+
+struct Node {
+    int l, r;
+    int sum; //当前段的区间和
+    int lmax, rmax; //子节点最大前后缀
+    int tmax; //当前段的最大子段和
+} tr[N * 4];
+
+void pushup(Node &u, Node &l, Node & r) {
+    u.sum = l.sum + r.sum;
+    u.lmax = max(l.lmax, l.sum + r.lmax); //维护
+    u.rmax = max(r.rmax, r.sum + l.rmax);
+    u.tmax = max(max(l.tmax, r.tmax), l.rmax + r.lmax);
+}
+
+void pushup(int u) {
+    pushup(tr[u], tr[u << 1], tr[u << 1 | 1]);
+}
+
+void build(int u, int l, int r) {
+    tr[u].l = l, tr[u].r = r;
+    if (l == r) tr[u] = {l, r, w[r], w[r], w[r], w[r]};
+    else {
+        int mid = l + r >> 1;
+        build(u << 1, l, mid), build(u << 1 | 1, mid + 1, r);
+        pushup(u);
+    }
+}
+
+int modify(int u, int x, int v) {
+    if (tr[u].l == x && tr[u].r == x) tr[u] = {x, x, v, v, v, v};
+    else {
+        int mid = tr[u].l + tr[u].r >> 1;
+        if (x <= mid) modify(u << 1, x, v);
+        else modify(u << 1 | 1, x, v);
+        pushup(u);
+    }
+}
+
+Node query(int u, int l, int r) {
+    if (tr[u].l >= l && tr[u].r <= r) return tr[u];
+    else {
+        int mid = tr[u].l + tr[u].r >> 1;
+        if (r <= mid) return query(u << 1, l, r);
+        else if (l > mid) return query(u << 1 | 1, l , r);
+        else {
+            auto left = query(u << 1, l , r);
+            auto right = query(u << 1 | 1, l, r);
+            Node res;
+            pushup(res, left, right);
+            return res;
+        }
+    }
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++) scanf("%d", &w[i]);
+    build(1, 1, n);
+
+    int k, x, y;
+    while (m --) {
+        scanf("%d%d%d", &k, &x, &y);
+        if (k == 1) {
+            if (x > y) swap(x, y);
+            printf("%d\n", query(1, x, y).tmax);
+        }
+        else modify(1, x, y);
+    }
+
+    return 0;
+}
