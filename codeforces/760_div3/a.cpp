a3495a93031cd337bb113ad16f47a26538431de8
Mona Lisa <45538642+livrth@users.noreply.github.com>
2021-12-15 01:39:11 +0800

    Codeforces Round #760 (Div. 3) 待补题

---
+++ codeforces/760_div3/a.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define um unordered_map
+#define pq priority_queue
+#define sz(x) ((int)(x).size())
+#define x first
+#define y second
+#define endl '\n'
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+void solve() {
+    ll a[7];
+    set<ll> S;
+    for (int i = 0; i < 7; i++) {
+        cin >> a[i];
+        S.insert(a[i]);
+    }
+    for (int i = 0; i < 7; i++)
+        for (int j = i + 1; j < 7; j++)
+            for (int k = j + 1; k < 7; k++) {
+                if (S.count(a[i] + a[j]) && S.count(a[j] + a[k]) && S.count(a[i] + a[k]) && a[i] + a[j] + a[k] == a[6]) {
+                    cout << a[i] << ' ' << a[j] << ' ' << a[k] << ' ' << endl;
+                    return;
+                }
+            }
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    while (t --) solve();
+    return 0;
+}
