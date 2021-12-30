33b1764c831f45558375ab9a975370ceb54ab735
Mona Lisa <45538642+livrth@users.noreply.github.com>
2021-12-31 00:09:15 +0800

    AtCoder Beginner Contest 232

---
+++ atcoder/abc232/d.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define um unordered_map
+#define pq priority_queue
+#define sz(x) ((int)(x).size())
+#define x first
+#define y second
+#define endl '\n'
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+
+//倒序 DP
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector c(n, vector<char>(m));
+    for (auto &v : c)
+        for (char &x : v)
+            cin >> x;
+    vector f(n + 1, vector<int>(m + 1));
+    for (int i = n - 1; i >= 0; i--)
+        for (int j = m - 1; j >= 0; j--) {
+            if (c[i][j] ==  '#')continue;
+            f[i][j] = max(f[i + 1][j], f[i][j + 1]) + 1;
+        }
+    cout << f[0][0] << endl;
+}
+
+int main() {
+    int t = 1;
+    // cin >> t;
+    while (t --) solve();
+    return 0;
+}
