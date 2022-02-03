60b0ce5c499e2105f1a947a0ecf934ab19840c6c
Mona Lisa <45538642+livrth@users.noreply.github.com>
2022-02-03 23:06:07 +0800

    自然数拆分/完全背包

---
+++ 动态规划/背包/acwing_279.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define um unordered_map
+#define pq priority_queue
+#define sz(x) ((int)(x).size())
+#define fi first
+#define se second
+#define endl '\n'
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 2147483648;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+//完全背包
+//至少拆成2个数注意 最后减去 x = x 这一种方案
+
+const int M = 4010;
+ll f[M];
+
+void solve() {
+    int n;
+    cin >> n;
+    f[0] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = i; j <= n; j++)
+            f[j] = (f[j] + f[j - i]) % mod; //v[i] == i 体积就是 i
+    cout << (f[n] + mod) % mod - 1 << endl;
+}
+
+int main() {
+    int t = 1;
+    // cin >> t;
+    while (t --) solve();
+    return 0;
+}
