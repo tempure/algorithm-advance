7e892ef7d0e3d1fa6545362990140060c26a66c4
Mona Lisa <45538642+livrth@users.noreply.github.com>
2022-03-12 14:50:20 +0800

    AtCoder Beginner Contest 181

---
+++ atcoder/abc181/d.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define um unordered_map
+#define pq priority_queue
+#define sz(x) ((int)(x).size())
+#define fi first
+#define se second
+#define endl '\n'
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+mt19937 mrand(random_device{}());
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x;}
+ll mulmod(ll a, ll b) {ll res = 0; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = (res + a) % mod; a = 2 * a % mod;} return res;}
+ll powmod(ll a, ll b) {ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) {if (b & 1)res = res * a % mod; a = a * a % mod;} return res;}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a;}
+//head
+
+//只需要判断末尾三位就可以了
+//https://atcoder.jp/contests/abc181/editorial/297
+
+bool solve(string s) {
+    if (s.size() == 1) return s == "8";
+    if (s.size() == 2) {
+        if (stoi(s) % 8 == 0) return 1;
+        swap(s[0], s[1]);
+        return stoi(s) % 8 == 0;
+    }
+    vector<int> cnt(10);
+    for (char x : s) cnt[x - '0']++;
+    for (int i = 112; i < 1000; i += 8) {
+        auto c = cnt;
+        for (char x : to_string(i)) c[x - '0']--;
+        if (all_of(c.begin(), c.end(), [](int x) { return x >= 0; })) return 1;
+    }
+    return 0;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    puts(solve(s) ? "Yes" : "No");
+}
