18824f5c6695bed7f18e64088fc57c38dcafd005
Mona Lisa <lyvrth@gmail.com>
2022-09-07 17:38:50 +0800

    add: pe 61

---
+++ projecteuler/level03/61.py
@@ -0,0 +1,46 @@
+# Polygonal number https://en.wikipedia.org/wiki/Polygonal_number
+# https://pe.metaquant.org/pe061.html
+# DFS find a simple circuit of length 6 in a sextogram
+# ans = 28684
+
+from collections import namedtuple
+
+def poly_numbers(s,n):
+    Pn = namedtuple('Pn','s v ftd ltd')
+    v = (n**2*(s-2)-n*(s-4))//2
+    ftd,ltd = v // 100, v % 100
+    pn = Pn(s,v,ftd,ltd)
+    return pn
+
+def poly_number_graph():
+    arr,graph = [],{}
+    for s in range(3,9):
+        for n in range(19,141):
+            pn = poly_numbers(s,n)
+            if 999 < pn.v < 10000 and pn.ltd>9:
+                arr.append(pn)
+    for pn in arr:
+        res = []
+        for apn in arr:
+            if pn.s != apn.s and pn.ltd == apn.ftd:
+                res.append(apn)
+        graph[pn] = res
+    return graph        
+
+def dfs(graph,types,numbers):
+    if len(types) == 6 and numbers[-1].ltd == numbers[0].ftd:
+        print(sum([x.v for x in numbers]))
+        return True
+    else:
+        for pn in graph.get(numbers[-1],[]):
+            if pn.s not in types:
+                dfs(graph,types+[pn.s],numbers+[pn])
+
+def main():
+    graph = poly_number_graph()
+    for pn in graph:
+        if(dfs(graph,[pn.s],[pn])):
+            return
+
+if __name__ == '__main__':
+    main()
