2f2993180fb21b65ca9920abb1b94d917765069d
Mona Lisa <lyvrth@gmail.com>
2022-09-13 23:28:54 +0800

    add: pe 81

---
+++ projecteuler/level04/81.py
@@ -0,0 +1,25 @@
+# 线性DP裸题
+# https://pe.metaquant.org/pe081.html
+# ans =  427337
+
+from functools import lru_cache
+
+with open('../data/p081_matrix.txt') as f:
+    table = [list(map(int,s.split(','))) for s in f.readlines()]
+
+@lru_cache(maxsize=2048)
+def min_path_sum(r,c):
+    if r == 0 and c == 0:
+        return table[0][0]
+    elif r == 0 and c >= 1:
+        return sum(table[r][:c+1])
+    elif c == 0 and r >= 1:
+        return sum(x[0] for x in table[:r+1])
+    else:
+        return table[r][c] + min(min_path_sum(r-1,c),min_path_sum(r,c-1))  
+
+def main(N=80):
+    return min_path_sum(N-1,N-1)
+
+if __name__ == '__main__':
+    print(main())
